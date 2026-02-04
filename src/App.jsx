49c41ebc638ec8e333af5e1949497a14b50a45b0
WorldWeblogic <sandeep21.wwl@gmail.com>
2026-02-04 16:14:56 +0530

    about1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import Home from './pages/Home'
-import Header from './components/Header'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import WhoWeAre from "./pages/about/whoWeAre";
 
 function App() {
-
   return (
-    <>
+    <Router>
       <Header />
-      <Home />
-    </>
-  )
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about/who-we-are" element={<WhoWeAre />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
