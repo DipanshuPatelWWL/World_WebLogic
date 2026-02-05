e5fc034f18988a6660488d78579fbe4a7cdb154e
ruchiwwl <ruchi.wwl@gmail.com>
2026-02-05 13:58:33 +0530

    header update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import WhoWeAre from "./pages/about/whoWeAre";
 import ContactUs from "./pages/ContactUs";
 import ScrollToTop from "./components/ScrollToTop";
+import SearchEngineOptimization from "./pages/Services/SEO";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about/who-we-are" element={<WhoWeAre />} />
+        <Route path="/services/seo" element={<SearchEngineOptimization />} />
         <Route path="/contact" element={<ContactUs />} />
       </Routes>
       <Footer />
