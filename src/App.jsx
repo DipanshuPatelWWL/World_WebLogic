643ac177ab4bd6edebc128f95a235de7ae6e168b
jahidvicky <jahidvicky@gmail.com>
2026-02-05 14:34:56 +0530

    create project section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import WhoWeAre from "./pages/about/whoWeAre";
 import ContactUs from "./pages/ContactUs";
 import ScrollToTop from "./components/ScrollToTop";
 import WhyWwl from "./pages/about/WhyWwl";
+import ProjectDetails from "./pages/ProjectDetails";
+import Projects from "./pages/Project";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
         <Route path="/about/who-we-are" element={<WhoWeAre />} />
         <Route path="/about/why-choose-us" element={<WhyWwl />} />
         <Route path="/contact" element={<ContactUs />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:slug" element={<ProjectDetails />} />
       </Routes>
       <Footer />
     </Router>
